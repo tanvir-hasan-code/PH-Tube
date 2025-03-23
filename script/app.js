5b87c285c32464a12a8d449e0d08c2f3095d17cb
tanvir-hadan-code <tanvirx015@gmail.com>
2025-03-24 03:44:10 +0600

    Update code

--- script/app.js
+++ script/app.js
@@ -42,7 +42,7 @@ function displayVideo(videos) {
 	if (videos.length === 0) {
 		videoContainer.innerHTML = `
 		<div class="md:w-11/12 mx-auto grid justify-center md:col-span-4 py-16">
-			<img class="mx-auto" src="../assets/Icon.png" alt="Oops-Icon">
+			<img class="mx-auto" src="assets/Icon.png" alt="Oops-Icon">
 			<h3 class="text-center  mt-3 text-2xl font-bold">Oops!! Sorry, There is no <br> content here</h3>
 		 </div>
 		`
